450210e384bb136d03da991972016c67cd85b13c
Ericchuk <47712998+Ericchuk@users.noreply.github.com>
2022-06-21 23:39:47 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 let adviceIndex = document.querySelector("small");
 let advice = document.querySelector("p");
 let dice = document.querySelector(".dice");
-let xmlhttp = new XMLHttpRequest();
-function generate(){
-       let  response = JSON.parse(xmlhttp.responseText);
-       adviceIndex.textContent = response.slip.id;
-        advice.textContent = response.slip.advice;
-        console.log(response.slip.advice);
-        console.log(xmlhttp.responseText);
+// let xmlhttp = new XMLHttpRequest();
+// xmlhttp.onload = function(){
+//        let  response = JSON.parse(xmlhttp.responseText);
+//        adviceIndex.textContent = response.slip.id;
+//         advice.textContent = `"${response.slip.advice}"`;
+//         adviceIndex.textContent = response.slip.id;
+//         dice.onclick = function(){
+//               window.location.reload();
+//               console.log("www");
+//               advice.textContent = response.slip.advice;
+//               adviceIndex.textContent = response.slip.id;
+//         }
+       
+// }
+
+// xmlhttp.send();
+
+let url = "https://api.adviceslip.com/advice";
+
+async function getData(){
+       let response = await fetch(url)
+       let result = await response.json();
+       // console.log(result);
+       advice.textContent = `"${result.slip.advice}"`;
+       adviceIndex.textContent = result.slip.id;
 }
 
-xmlhttp.open("get", "https://api.adviceslip.com/advice");
-xmlhttp.send();
-
-// generate();
-dice.addEventListener("click", generate);
+getData();
+dice.addEventListener("click", getData);
